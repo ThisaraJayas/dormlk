57c7c9fe866346ed7b53fc80a9a8a958afbcc83d
Thisara <sasmithajayasinghe1@gmail.com>
2024-07-07 23:43:02 +0530

    jwt

---
+++ src/main/java/lk/dorm/dormlk/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package lk.dorm.dormlk.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="fjdfndjnfdjndjgbdkfndkgndjgdkkdjgdkjksehk";
+    public static final String JWT_HEADER="Authorization";
+}

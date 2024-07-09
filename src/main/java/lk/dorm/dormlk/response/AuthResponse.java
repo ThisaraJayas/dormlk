94f6ffcac4f48956ad03fb6d3f6bc420d0725381
Thisara <sasmithajayasinghe1@gmail.com>
2024-07-09 23:04:10 +0530

    response

---
+++ src/main/java/lk/dorm/dormlk/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package lk.dorm.dormlk.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String jwt;
+    public String message;
+}

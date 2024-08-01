a4d03eab1ff7ec02b7b67fd61f0fc6f3daf03fc0
Thisara <sasmithajayasinghe1@gmail.com>
2024-08-01 22:48:45 +0530

    admin controller

--- src/main/java/lk/dorm/dormlk/controller/AdminController.java
+++ src/main/java/lk/dorm/dormlk/controller/AdminController.java
@@ -1,10 +1,27 @@
 package lk.dorm.dormlk.controller;
 
+import lk.dorm.dormlk.entity.Post;
+import lk.dorm.dormlk.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/allPosts")
+    public ResponseEntity<List<Post>> getAllPosts(){
+        List<Post> posts = postService.getAllPosts();
+        return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
 
 }

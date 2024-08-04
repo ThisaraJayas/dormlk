bbce7fc3c23606dafef266980a4454630ca9b1eb
Thisara <sasmithajayasinghe1@gmail.com>
2024-08-04 13:26:26 +0530

    comment entity

---
+++ src/main/java/lk/dorm/dormlk/entity/Comment.java
@@ -0,0 +1,24 @@
+package lk.dorm.dormlk.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String content;
+    private LocalDateTime createdDateTime;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Post post;
+
+
+}

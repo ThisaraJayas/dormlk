40d5f9a1f452df75070f3ec09e62e841ee8c14e9
Thisara <sasmithajayasinghe1@gmail.com>
2024-08-08 22:47:27 +0530

    reply

--- src/main/java/lk/dorm/dormlk/request/CreateReplyRequest.java
+++ src/main/java/lk/dorm/dormlk/request/CreateReplyRequest.java
@@ -1,4 +1,13 @@
 package lk.dorm.dormlk.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateReplyRequest {
+    private Long messageId;
+    private String message;
 }

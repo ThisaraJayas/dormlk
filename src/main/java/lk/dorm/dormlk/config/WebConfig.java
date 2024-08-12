3b3dbbc0c06c828ac7053bf2a5991ae548096e5a
Thisara <sasmithajayasinghe1@gmail.com>
2024-08-12 19:30:50 +0530

    dockerfile

--- src/main/java/lk/dorm/dormlk/config/WebConfig.java
+++ src/main/java/lk/dorm/dormlk/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Update with your frontend URL
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://dormlk-frontend-1anh.vercel.app"
+                ) // Update with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600);
